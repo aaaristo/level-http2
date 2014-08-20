e3b2e147a74670fac20702c3657526af0b645f05
Andrea Gariboldi <andrea@andreas-air.lan>
2014-08-20 16:55:00 +0200

    tests

--- test/client.js
+++ test/client.js
@@ -1,4 +1,5 @@
-var lhttp2= require('..');
+var lhttp2= require('..'),
+    async= require('async');
 
 var should= require('chai').should(),
     assert= require('chai').assert;
@@ -9,8 +10,16 @@ describe('client',function ()
 
        before(function (done)
        {
-  
-           lhttp2.server().listen(8080,done);
+           var server= lhttp2.server();
+           server.db.createKeyStream()
+                    .on('data',function (key)
+                    {
+                         server.db.del(key);
+                    })
+                    .on('end',function ()
+                    { 
+                       server.listen(8080,done);
+                    });
        });
 
        it('can put a kv',function (done)
@@ -40,5 +49,58 @@ describe('client',function ()
               'NotFoundError'.should.equal(err.type);
               done();
           });
+       });
+
+       var expected= [{ key: 'andrea', value: new Buffer('andrea','utf8') },
+                      { key: 'nico', value: new Buffer('nico','utf8') }];
+
+       it('can read all the data', function (done)
+       {
+
+          async.forEach(expected,
+          function (data,done)
+          {
+             client.put(data.key,data.value,done);
+          },
+          function (err)
+          {
+              if (err) return done(err);
+
+              var exp= expected.slice();
+            
+              client.createReadStream()
+                    .on('error',done)
+                    .on('data',function (data)
+                     {
+                         exp.shift().should.eql(data)
+                     })
+                    .on('end',done);
+          });
+       });
+
+       it('can read all keys', function (done)
+       {
+              var exp= expected.slice();
+
+              client.createKeyStream()
+                    .on('error',done)
+                    .on('data',function (data)
+                     {
+                         exp.shift().key.should.eql(data)
+                     })
+                    .on('end',done);
+       });
+
+       it('can read all values', function (done)
+       {
+              var exp= expected.slice();
+
+              client.createValueStream()
+                    .on('error',done)
+                    .on('data',function (data)
+                     {
+                         exp.shift().value.should.eql(data)
+                     })
+                    .on('end',done);
        });
 });
